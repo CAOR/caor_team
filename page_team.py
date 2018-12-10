c980489bfe24f5e400db9dced067fe7195393711
Amaury Bréhéret <abreheret@gmail.com>
2018-12-10 18:03:48 +0100

    add room to test

--- page_team.py
+++ page_team.py
@@ -79,6 +79,8 @@ def make_section(name,persons):
             out += '<a href="'+p['github']+'" target="_blank"><i class="fa fa-github-square"></i></a> &nbsp;&nbsp;'
         if 'vimeo' in p.keys() and p['vimeo'] != '':
             out += '<a href="'+p['vimeo']+'" target="_blank"><i class="fa fa-vimeo-square"></i></a> &nbsp;&nbsp;'
+        if 'room' in p.keys() and p['room'] != '':
+            out += '<a href="'+p['room']+'" target="_blank"><i class="fa fa-desktop"></i></a> &nbsp;&nbsp;'
         out += '</p> </div> </div> </div> </div>\n'
     out += '</div> </div> </div>'
     #out = out.replace("\n    ","")
