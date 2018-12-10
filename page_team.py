ed5fc09c4539760d059fb4609a3ef75bdf16a1bb
Amaury Bréhéret <abreheret@gmail.com>
2018-12-10 18:19:33 +0100

    update tooltips ROOM

--- page_team.py
+++ page_team.py
@@ -47,7 +47,7 @@ def get_fr_to_en(status):
 
 def make_section(name,persons):
     since = get_fr_to_en(u'depuis')
-    print(name)
+    # print(name)
     out = '<div class="section_big_title"> <h1><span> '+name+'</span></h1>'
     out +='</div> <div class="container"> <div class="row"> <div class="sixteen columns">'
     for p in persons : 
@@ -89,7 +89,56 @@ def make_section(name,persons):
 
 def print_header() :
     out = "<link href=\"//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css\" rel=\"stylesheet\">"
-    out += "<style> .team_block_content .pic img { border-radius: 5%; } </style>"
+    out += """
+    <style> .team_block_content .pic img { border-radius: 5%; } 
+    /* Tooltip container */
+    .tooltip {
+      position: relative;
+      display: inline-block;
+      border-bottom: 1px dotted black; /* If you want dots under the hoverable text */
+    }
+
+    /* Tooltip text */
+    .tooltip .tooltiptext {
+      visibility: hidden;
+      width: 120px;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 6px;
+
+      /* Position the tooltip text */
+      position: absolute;
+      z-index: 1;
+      bottom: 125%;
+      left: 50%;
+      margin-left: -60px;
+
+      /* Fade in tooltip */
+      opacity: 0;
+      transition: opacity 0.3s;
+    }
+
+    /* Tooltip arrow */
+    .tooltip .tooltiptext::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: #555 transparent transparent transparent;
+    }
+
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip:hover .tooltiptext {
+      visibility: visible;
+      opacity: 1;
+    }
+    </style>
+    """
     out += "\n\n"
     return out
 
